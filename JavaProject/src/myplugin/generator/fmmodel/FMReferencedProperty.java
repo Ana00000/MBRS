028514c6db7525f3e5bb0086bca58f343cd089f3
aca24 <acasavic2409@gmail.com>
2022-09-26 10:53:16 +0200

    added method and parameter class

--- JavaProject/src/myplugin/generator/fmmodel/FMReferencedProperty.java
+++ JavaProject/src/myplugin/generator/fmmodel/FMReferencedProperty.java
@@ -1,17 +1,27 @@
 package myplugin.generator.fmmodel;
 
 public class FMReferencedProperty extends FMProperty {
-	private FetchType fetchType;
-	private CascadeType cascadeType;
+	private String fetchType;
+	private String cascadeType;
 	private String columnName;
 	private String joinTable;
-	private FMReferencedProperty oppositeEnd;
+	private Integer oppositeEnd;
 	
 	public FMReferencedProperty(FMProperty fmProperty) {
 		super(fmProperty.getName(), fmProperty.getType(), fmProperty.getVisibility(),fmProperty.getLower(), fmProperty.getUpper());
 	}
 
-	public FMReferencedProperty(FMProperty fmProperty, FetchType fetchType, CascadeType cascadeType, String columnName, 
+	public FMReferencedProperty(String name, String type, String visibility, int lower, int upper, String cascade,
+			String fetch, String joinTable, String columnName, Integer oppositeEnd) {
+		super(name, type, visibility, lower, upper);
+		this.cascadeType = cascade;
+		this.fetchType = fetch;
+		this.joinTable = joinTable;
+		this.columnName = columnName;
+		this.oppositeEnd = oppositeEnd;
+	}
+	
+	/*public FMReferencedProperty(FMProperty fmProperty, FetchType fetchType, CascadeType cascadeType, String columnName, 
 			String joinTable, FMReferencedProperty oppositeEnd) {
 		super(fmProperty.getName(), fmProperty.getType(), fmProperty.getVisibility(),fmProperty.getLower(), fmProperty.getUpper());
 		this.fetchType = fetchType;
@@ -19,45 +29,56 @@ public class FMReferencedProperty extends FMProperty {
 		this.columnName = columnName;
 		this.joinTable = joinTable;
 		this.oppositeEnd = oppositeEnd;
+	}*/
+
+	public Integer getOppositeEnd() {
+		return oppositeEnd;
 	}
 
-	public FetchType getFetchType() {
-		return fetchType;
+
+
+	public void setOppositeEnd(Integer oppositeEnd) {
+		this.oppositeEnd = oppositeEnd;
 	}
 
-	public void setFetchType(FetchType fetchType) {
-		this.fetchType = fetchType;
-	}
 
-	public CascadeType getCascadeType() {
+
+	public String getCascade() {
 		return cascadeType;
 	}
 
-	public void setCascadeType(CascadeType cascadeType) {
-		this.cascadeType = cascadeType;
+
+	public void setCascade(String cascade) {
+		this.cascadeType = cascade;
 	}
 
-	public String getColumnName() {
-		return columnName;
+
+	public String getFetch() {
+		return fetchType;
 	}
 
-	public void setColumnName(String columnName) {
-		this.columnName = columnName;
+
+	public void setFetch(String fetch) {
+		this.fetchType = fetch;
 	}
+
 
 	public String getJoinTable() {
 		return joinTable;
 	}
 
+
 	public void setJoinTable(String joinTable) {
 		this.joinTable = joinTable;
 	}
-	
-	public FMReferencedProperty getOppositeEnd() {
-		return this.oppositeEnd;
+
+
+	public String getColumnName() {
+		return columnName;
 	}
-	
-	public void setOppositeEnd(FMReferencedProperty oppositeEnd) {
-		this.oppositeEnd = oppositeEnd;
+
+
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
 	}
 }
